35721cba7d90dbfd193691be40be32c0ccf92490
Kabir Pal <kabirpal53@gmail.com>
2024-09-06 20:55:12 +0530

    Animation added

--- src/App.js
+++ src/App.js
@@ -10,10 +10,16 @@ import code from "./assets/images/code.png";
 import consulting from "./assets/images/consulting.png";
 import project1 from "./assets/images/web1.png";
 import { useEffect, useState } from "react";
+import { TypeAnimation } from 'react-type-animation';
 
 function App() {
 	const [darkMode, setDarkMode] = useState(false);
 	useEffect(() => {}, [darkMode]);
+	const openGmailWithCC = () => {
+		const ccEmail = 'kabirpal53@gmail.com';
+		const gmailLink = `https://mail.google.com/mail/u/0/?fs=1&tf=cm&cc=${encodeURIComponent(ccEmail)}`;
+		window.open(gmailLink, '_blank');
+	  };
 	return (
 		<div className={darkMode ? "dark" : ""}>
 			<main className="bg-white px-10 md:px-20 lg:px-30 dark:bg-gray-600 dark:text-white">
@@ -43,26 +49,18 @@ function App() {
 						<h2 className="text-5xl text-teal-500 font-medium md:text-6xl">
 							Kabir Pal
 						</h2>
-						<h3 className="text-2xl py-2 md:text-3xl">
+						<h3 className="text-2xl pt-5 md:text-3xl animate-bounce animate-infinite">
 							Software Engineer
 						</h3>
-						<p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto">
-							Software Engineer Software Engineer Software
-							Engineer Software Engineer Software Engineer
-							Software Engineer Software Engineer Software
-							Engineer Software Engineer Software Engineer
-							Software Engineer
-                            Software Engineer Software Engineer Software
-							Engineer Software Engineer Software Engineer
-							Software Engineer Software Engineer Software
-							Engineer Software Engineer Software Engineer
-							Software Engineer
+						<p className="text-md pt-2 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto">
+						Software Engineer proficient in <b>React, Angular, JavaScript, TypeScript, MySQL, Redux,</b> and <b>RxJs</b>. 
+						Familiar with <b>Nodejs, Python, Firebase</b>. Skilled in Front End Web Development.
 						</p>
 					</div>
 					<div className="flex text-5xl justify-center text-gray-500">
-						<AiFillLinkedin></AiFillLinkedin>
-						<AiFillMail></AiFillMail>
-						<AiFillGithub></AiFillGithub>
+						<a href="https://www.linkedin.com/in/kabirpal/"><AiFillLinkedin className="hover:text-gray-800"></AiFillLinkedin></a>
+						<AiFillMail className="hover:text-gray-800 hover:cursor-pointer" onClick={openGmailWithCC}></AiFillMail>
+						<a href="https://github.com/kabirpal/"><AiFillGithub className="hover:text-gray-800"></AiFillGithub></a>
 					</div>
 					<div className="relative mx-auto bg-gradient-to-b from-teal-300 rounded-full w-80 h-80 mt-10 pt-5">
 						<img src={profileImage} />
@@ -71,9 +69,22 @@ function App() {
 
 				<section>
 					<div className="">
-						<h3 className="text-3xl py-1">
-							Technologies I worked with
-						</h3>
+					<TypeAnimation
+						sequence={[
+							'Technologies I worked with React',
+							1000,
+							'Technologies I worked with Angular',
+							1000,
+							'Technologies I worked with Redux',
+							1000,
+							'Technologies I worked with HTML/CSS',
+							1000
+						]}
+						wrapper="span"
+						speed={50}
+						style={{ fontSize: '2em', display: 'inline-block' }}
+						repeat={Infinity}
+						/>
 						<p className="text-md py-5 leading-8 text-gray-800">
 							Lorem ipsum dolor sit amet consectetur, adipisicing
 							elit. Accusamus officiis ipsa deserunt nostrum nam
